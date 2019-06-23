246c8612510698eec3dbc0e7b297312dd18c6242
fangqk1991 <me@fangqk.com>
2019-06-23 23:12:06 +0800

    support relative path of config file

--- cli.js
+++ cli.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
 const assert = require('assert')
+const path = require('path')
 const Project = require('./lib/Project')
 
 const [,, ...args] = process.argv
 assert.ok(Array.isArray(args) && args[0], `Please use command: mini-publish YOUR-CONFIG-FILE-PATH`)
-const project = new Project(args[0])
+
+const configFile = path.resolve(process.cwd(), args[0])
+const project = new Project(configFile)
 project.build()
