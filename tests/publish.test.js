23894e7db1b51d4d04aa5b683d16ec1747dabdb8
fangqk1991 <me@fangqk.com>
2019-06-24 09:34:52 +0800

    global bin dir fixed

--- tests/publish.test.js
+++ tests/publish.test.js
@@ -1,10 +1,18 @@
 const Project = require('../lib/Project')
+const shell = require('shelljs')
 const assert = require('assert')
+
+describe('Test HomeDir', () => {
+  it(`Shell`, () => {
+    shell.cd('~')
+    const homeDir = shell.pwd().stdout
+    assert.ok(!!homeDir)
+  })
+})
 
 describe('Test Publish', () => {
   it(`Publish`, () => {
     const project = new Project(`${__dirname}/config-demo.js`)
-    assert.ok(project)
     project.build()
   })
 })
